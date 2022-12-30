55318ed1acd4eec87327279ea1bcb16b3403eed7
Kajkac <kristijan.poljanec@gmail.com>
2022-12-30 09:47:44 +0100

    Update run.py

--- zte/run.py
+++ zte/run.py
@@ -1,6 +1,6 @@
-import datetime
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
+import requests
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
     ATTR_FRIENDLY_NAME,
@@ -8,23 +8,18 @@ from homeassistant.const import (
 )
 from homeassistant.helpers.entity import Entity
 
-CONF_SHOW_SECONDS = 'show_seconds'
-
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_NAME, default="Current Time"): cv.string,
-    vol.Optional(CONF_SHOW_SECONDS, default=False): cv.boolean
+    vol.Optional(CONF_NAME, default="Public IP Address"): cv.string,
 })
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     name = config.get(CONF_NAME)
-    show_seconds = config.get(CONF_SHOW_SECONDS)
 
-    add_entities([CurrentTimeSensor(name, show_seconds)])
+    add_entities([PublicIPSensor(name)])
 
-class CurrentTimeSensor(Entity):
-    def __init__(self, name, show_seconds):
+class PublicIPSensor(Entity):
+    def __init__(self, name):
         self._name = name
-        self._show_seconds = show_seconds
         self._state = None
 
     @property
@@ -36,9 +31,5 @@ class CurrentTimeSensor(Entity):
         return self._state
 
     def update(self):
-        now = datetime.datetime.now()
-        if self._show_seconds:
-            self._state = now.strftime("%H:%M:%S")
-        else:
-            self._state = now.strftime("%H:%M")
-
+        response = requests.get("https://api.ipify.org")
+        self._state = response.text
